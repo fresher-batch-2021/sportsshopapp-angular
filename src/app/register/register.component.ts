8c0b440db520c07527b0f212974b63f09f33b4a2
Sudharsanan R <Sudharsanan111@gmail.com>
2021-08-10 17:36:21 +0530

    new update

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,5 +11,32 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
   }
+    name:string = ""; 
+email:string = "";
+pwd:string="";
+cpwd:string="";
+
+login(){
+  if(this.name==""){
+    alert("Name Cannot blank");
+  }
+
+  if(this.email==""){
+    alert("Email Cannot Blank");
+  }
+  
+  if(this.pwd.length<8){
+    alert("cannot blank");
+  }
+
+  if(this.pwd!=this.cpwd){
+    alert("Wrong Password");
+  }
+
+  else{
+    alert("successfull");
+    window.location.href="listtask";
+  }
+  }
 
 }
